faf97481e856b2e02d1025c434043db1c2445b1c
Desa Garut <desagarut.net@gmail.com>
2025-11-12 12:28:51 +0700

    Update practice_area.php

--- themes/lawmaker/partials/practice_area.php
+++ themes/lawmaker/partials/practice_area.php
@@ -5,7 +5,7 @@
                 <h2>Layanan Hukum</h2>
             </div>
         </div>
-        <div class="row">
+        <div class="row text-center">
             <div class="col-md-4 text-center animate-box">
                 <div class="services">
                     <span class="icon">
